3ebd4a87c2cd238ad21a62521d5e0c24b1a84525
LetsB <55174288+LetsB@users.noreply.github.com>
2019-11-25 14:06:11 +0900

    Add files via upload

---
+++ Quiz/sketch_nov06a/sketch_nov06a.ino
@@ -0,0 +1,41 @@
+int sensorPosPin = A2; // input pin for MR sensor
+int rawPos;
+int t;
+bool mode;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);//9600
+  pinMode(sensorPosPin, INPUT); // set MR sensor pin to be an input
+  mode = true;
+  t = 0;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int inByte = Serial.read();
+  if(inByte == '1')
+    mode = true;
+  else if(inByte == '2')
+    mode = false;
+
+    if(mode)
+      calculatePosition();
+    else
+      calculateVirtualPosition();
+  Serial.println(rawPos);
+  delay(20);
+}
+
+
+void calculatePosition()
+{
+  rawPos = analogRead(sensorPosPin);
+}
+
+void calculateVirtualPosition()
+{
+  t = t+1;
+  rawPos = 512 + 512.0 * sin(0.1 * t);
+}
+
